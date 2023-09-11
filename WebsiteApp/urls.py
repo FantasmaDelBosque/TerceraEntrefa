def173bdff8f66115acea1340aacf764eadc03ce
Isabel <isabel@iMac>
2023-09-11 00:28:00 -0600

    herencias

--- WebsiteApp/urls.py
+++ WebsiteApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import  inicio, eventos, discografia, biografia
 
 urlpatterns = [
-    path('', inicio),
-    path('eventos/', eventos),
-    path('discografia/', discografia),
-    path('biografia/', biografia)
+    path('', inicio, name="Inicio"),
+    path('eventos/', eventos, name="Eventos"),
+    path('discografia/', discografia, name="Discografia"),
+    path('biografia/', biografia, name="Biografia")
 ]
